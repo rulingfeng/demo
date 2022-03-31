6e8aacddaf8911f2121232839055b2301a82bfc2
茹凌丰 <512387589@qq.com>
2022-03-31 16:41:30 +0800

    redis

--- src/main/java/com/example/demo/redis/bitmap/RedisBitMapDemo.java
+++ src/main/java/com/example/demo/redis/bitmap/RedisBitMapDemo.java
@@ -14,6 +14,21 @@ public class RedisBitMapDemo {
     private RedisTemplate redisTemplate;
 
     //用户签到 key是用户id+年+月   offset是日   value 是 true（代表已签到）
+
+    public void demo() throws InterruptedException {
+        this.setBit("2022-03", 31,false);
+        boolean bit = this.getBit("2022-03", 31);//false
+
+        this.setBit("2022-03", 31,true);
+        boolean bit1 = this.getBit("2022-03", 31);//true
+
+
+        redisTemplate.expire("2022-03",5, TimeUnit.SECONDS);
+        TimeUnit.SECONDS.sleep(7);
+
+        boolean bit2 = this.getBit("2022-03", 31);//false
+    }
+
     /**
      * 设置key字段第offset位bit数值
      *
@@ -36,18 +51,27 @@ public class RedisBitMapDemo {
         return (boolean) redisTemplate.execute((RedisCallback) con -> con.getBit(key.getBytes(), offset));
     }
 
-
-    public void demo() throws InterruptedException {
-        this.setBit("2022-03", 31,false);
-        boolean bit = this.getBit("2022-03", 31);//false
-
-        this.setBit("2022-03", 31,true);
-        boolean bit1 = this.getBit("2022-03", 31);//true
-
-
-        redisTemplate.expire("2022-03",5, TimeUnit.SECONDS);
-        TimeUnit.SECONDS.sleep(7);
-
-        boolean bit2 = this.getBit("2022-03", 31);//false
+    /**
+     * 统计key字段value为1的总数
+     *
+     * @param key 字段
+     * @return 总数
+     */
+    public Long bitCount(String key) {
+        return (Long) redisTemplate.execute((RedisCallback<Long>) con -> con.bitCount(key.getBytes()));
     }
+
+    /**
+     * 统计key字段value为1的总数,从start开始到end结束
+     *
+     * @param key   字段
+     * @param start 起始
+     * @param end   结束
+     * @return 总数
+     */
+    public Long bitCount(String key, Long start, Long end) {
+        return (Long) redisTemplate.execute((RedisCallback) con -> con.bitCount(key.getBytes(), start, end));
+    }
+
+
 }
